60a5308186e96f230fb7316d987416599724c5d0
kageyamountain <kageyamountain@users.noreply.github.com>
2025-09-08 04:29:37 +0900

    add: ArticleList

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'article-list',
+      component: () => import('../page/article-list/ArticleList.vue'),
+    },
+    {
       path: '/sample',
       name: 'sample',
       component: () => import('../page/sample/index.vue'),
