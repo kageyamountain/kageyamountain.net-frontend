a2d92d7ad01adc1d270dbf9fa11ce42281db21eb
kageyamountain <kageyamountain@users.noreply.github.com>
2025-09-08 06:13:24 +0900

    ArticleListコンポーネント追加

---
+++ src/page/article-list/ArticleList.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import UserProfile from '@/page/article-list/component/UserProfile.vue'
+</script>
+
+<template>
+  <UserProfile />
+  <div class="bg-app-secondary">Article Cards</div>
+</template>
