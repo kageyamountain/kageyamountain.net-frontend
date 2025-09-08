a663812bc4496dc4791c04290b4216f119fd0407
kageyamountain <kageyamountain@users.noreply.github.com>
2025-09-09 08:23:49 +0900

    cleanup

--- src/share/component/header.vue
+++ src/share/component/header.vue
@@ -1,7 +1,7 @@
 <template>
-  <header class="bg-app-primary w-full h-[70px]">
-    <div class="text-white container mx-auto flex items-baseline h-full">
-      <router-link to="/" class="text-2xl mt-4">Kageyamountain.net</router-link>
+  <header class="bg-app-primary h-[70px] w-full">
+    <div class="container mx-auto flex h-full items-baseline text-white">
+      <router-link to="/" class="mt-4 text-2xl">kageyamountain.net</router-link>
       <div class="ml-10">
         <router-link to="/" active-class="underline underline-offset-2" class="">Home</router-link>
         <router-link to="/search" active-class="underline underline-offset-2" class="ml-6">Search</router-link>
