210b61696cbfe1dd39673668ff94a941df7dee54
kageyamountain <kageyamountain@users.noreply.github.com>
2025-09-15 08:48:48 +0900

    add: comment

--- src/share/util/article.ts
+++ src/share/util/article.ts
@@ -1,12 +1,17 @@
 import { TAG_COLORS, type TAG_COLORS_KEY } from "@/share/constant"
 
+/**
+ * タグ値に対応する色を取得する
+ * @param tag タグ値
+ * @returns 対応する色のクラス名
+ */
 export function getTagColor(tag: string): string {
   return TAG_COLORS[tag as TAG_COLORS_KEY] || "bg-gray-400"
 }
 
 /**
  * ISO8601 形式の文字列をローカルタイムの "yyyy-mm-dd" 形式に変換する
- * @param isoDate - ISO8601 形式の文字列
+ * @param isoDate ISO8601 形式の文字列
  * @returns ローカルタイムの日付文字列
  * @example
  * ```typescript
