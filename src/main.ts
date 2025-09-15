3340485709d1b777bae53970b27e16337df4a057
kageyamountain <kageyamountain@users.noreply.github.com>
2025-09-15 11:05:38 +0900

    lint自動修正

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
+import { createApp } from "vue"
 
-import App from './App.vue'
-import router from './router'
-import './tailwind.css'
+import App from "./App.vue"
+import router from "./router"
+import "./tailwind.css"
 
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+app.mount("#app")
